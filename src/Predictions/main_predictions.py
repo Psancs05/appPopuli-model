a62411450e4163d6f270cafb7fd1be64080f11e8
javier <psancs05@estudiantes.unileon.es>
2023-10-11 21:11:22 +0200

    files

---
+++ src/Predictions/main_predictions.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import os, sys
+
+PLAGAS = [
+    "Archips xylosteana",
+    "Cerura iberica",
+    "Chrysomela populi",
+    "Cossus cossus",
+    "Crepidodera spp",
+    "Cryptorhynchus lapathi",
+    "Cytospora chrysosperma",
+    "Dothichiza populea",
+    "Gypsonoma aceriana",
+    "Laothoe populi",
+    "Lepidosaphes ulmi",
+    "Leucoma salicis",
+    "Lonsdalea quercina subsp populi",
+    "Marssonina brunnea",
+    "Melampsora spp",
+    "Melanophila picta",
+    "Paranthrene tabaniformis",
+    "Pemphigus spp",
+    "Phloemyzus passerinii",
+    "Phratora laticolis",
+    "SANO",
+    "Saperda carcharias",
+    "Saperda populnea",
+    "Sesia apiformis",
+    "Taphrina populnea (Taphrina aurea)",
+    "Trypophloeus spp",
+    "Venturia populina",
+    "Xanthomonas populi",
+]
+
+PATH_MODEL = os.path.join('../','data', 'models', 'modelo.h5')
+
+def loadModel(path_model):
+    return  tf.keras.models.load_model(path_model)
+
+def resizeImages(path_image):
+    img = tf.io.read_file(path_image)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.resize(img, (224, 224))
+    img = tf.cast(img, tf.float32) / 255.0
+
+    return tf.expand_dims(img, axis=0)
+
+def main(path_image, path_model=PATH_MODEL):
+
+    # Carga el modelo
+    loaded_model = loadModel(path_model)
+
+    # Resize las dimensiones de la iamgen
+    input_data   = resizeImages(path_image)
+
+    # Realiza las predicciones del modelo
+    # print(loaded_model.predict(input_data))
+    logits = loaded_model.predict(input_data)
+    class_idx = tf.argmax(logits, axis=1).numpy()[0]
+    class_name = PLAGAS[class_idx]
+    return class_name
+
+
+if __name__ == '__main__' :
+
+    # argv[1] -> path de la imagen
+    # argv[2] -> path del modelo 
+
+    if len(sys.argv) == 2:
+        print(main(sys.argv[1]))
+    elif len(sys.argv) == 3:
+        print(main(sys.argv[1], sys.argv[2]))
+    else:
+        # main(os.path.join('data', 'ejemplo.jpg'))
+        print('El numero de parametros es incorrecto (1 o 2)')
+
+    
---
+++ src/backend.py
@@ -0,0 +1,52 @@
+from decimal import Decimal, ROUND_HALF_UP
+from flask import Flask, request, jsonify
+from gevent.pywsgi import WSGIServer
+import Predictions.main_predictions
+
+app = Flask(__name__)
+SCRIPT = '/Predictions/main_predictions.py'
+
+@app.errorhandler(405)
+def handle_method_not_allowed(e):
+    return jsonify({'estado': 'ko', 'resultado': 'Solo se admite el uso del método -POST- '}), 405
+
+@app.errorhandler(404)
+def handle_method_not_allowed(e):
+    return jsonify({'estado': 'ko', 'resultado': 'Solo se admite el uso del método -POST- '}), 404
+
+@app.errorhandler(500)
+def handle_method_not_allowed(e):
+    return jsonify({'estado': 'ko', 'resultado': 'El argumento ruta_img o ruta_model estan mal '}), 500
+
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        ruta_img = request.get_json().get('ruta_img')
+        ruta_model = request.get_json().get('ruta_model')
+
+        # print ruta_img and ruta_model
+        print("ruta_img: ",ruta_img)
+        print("ruta_model: ",ruta_model)
+
+        # Comprueba el parámetro
+        if ruta_img is None:
+            return jsonify({'estado': 'ko', 'resultado': 'El argumento ruta_img o ruta_model estan mal '}), 500
+
+        if ruta_model is None:
+            output = Predictions.main_predictions.main(ruta_img)
+        else:
+            output = Predictions.main_predictions.main(ruta_img,ruta_model)
+        
+        return jsonify({'estado': 'ok', 'resultado': output})
+    except Exception as e:
+        return jsonify({'estado': 'ko', 'resultado': 'Error en el engine' , 'excepcion': str(e)})
+    
+@app.route('/api', methods=['GET'])
+def ping():
+    return jsonify({'estado': 'ok', 'resultado': 'pong'})
+    
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(("127.0.0.1", 8080), app)
+    http_server.serve_forever()
